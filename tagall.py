e6697f05acd1521cdd158d318750239557615e8f
Demenkop <46030746+Demenkop@users.noreply.github.com>
2020-02-09 17:37:48 +0200

    Update tagall.py

--- tagall.py
+++ tagall.py
@@ -9,6 +9,6 @@ async def _(event):
     mentions = "@tagall"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
-        mentions += f"[\u2063](tg://user?id={x.id})"
+        mentions += f"(tg://user?id={x.id})"
     await event.reply(mentions)
     await event.delete()
