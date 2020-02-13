2e9f405224199ad5aa49c6a6810b01800ed8e3a1
Demenkop <46030746+Demenkop@users.noreply.github.com>
2020-02-13 17:06:02 +0200

    Create ding.py

---
+++ ding.py
@@ -0,0 +1,30 @@
+
+
+# Edited for FTg by demenkop
+
+
+@borg.on(events.NewMessage(pattern=r"\.ding", outgoing=True))
+
+async def _(event):
+    if event.fwd_from:
+        return
+
+    animation_interval = 0.3
+    animation_ttl = range(0, 10)
+    input_str = event.pattern_match.group(1)
+    animation_chars = [
+        
+            "🔴⬛⬛⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",
+            "⬜⬜⬛⬜⬜\n⬜⬛⬜⬜⬜\n🔴⬜⬜⬜⬜",
+            "⬜⬜⬛⬜⬜\n⬜⬜⬛⬜⬜\n⬜⬜🔴⬜⬜",
+            "⬜⬜⬛⬜⬜\n⬜⬜⬜⬛⬜\n⬜⬜⬜⬜🔴",
+            "⬜⬜⬛⬛🔴\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",    
+            "⬜⬜⬛⬜⬜\n⬜⬜⬜⬛⬜\n⬜⬜⬜⬜🔴",
+            "⬜⬜⬛⬜⬜\n⬜⬜⬛⬜⬜\n⬜⬜🔴⬜⬜",
+            "⬜⬜⬛⬜⬜\n⬜⬛⬜⬜⬜\n🔴⬜⬜⬜⬜",
+            "🔴⬛⬛⬜⬜\n⬜⬜⬜⬜⬜\n⬜⬜⬜⬜⬜",
+            "⬜⬜⬜⬜⬜\n⬜ [BOTHUB](https://github.com/mkaraniya/BotHub/) ⬜\n⬜⬜⬜⬜⬜"
+ ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 10])
