94eda8bc05571cf506d353753f2a77b3ad8ab502
Demenkop <46030746+Demenkop@users.noreply.github.com>
2020-03-31 03:51:35 +0300

    Update rtts.py

--- rtts.py
+++ rtts.py
@@ -29,7 +29,7 @@ async def _(event):
     async with event.client.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=616484527))
-              if not self_mess await event.client.forward_messages(chat, reply_message) else await event.client.send_message(chat.chat_id, reply_message "")
+              if not self_mess await event.client.forward_messages(chat, reply_message) else await event.client.send_message(chat.chat_id, reply_message.message)
               response = await response 
           except YouBlockedUserError: 
               await event.reply("```Разблокируй @aleksobot, ибо магия не произойдёт```")
