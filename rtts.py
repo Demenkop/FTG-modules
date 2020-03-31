511edf06b6d89e4ae4474e7f1739cb798d57c635
Demenkop <46030746+Demenkop@users.noreply.github.com>
2020-03-31 03:11:07 +0300

    Update rtts.py

--- rtts.py
+++ rtts.py
@@ -12,17 +12,16 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
-       return
-    reply_message = await event.get_reply_message() 
-    if not reply_message.text:
-       await event.edit("```Reply to text message```")
-       return
+        reply_message = event.pattern_match.group(1)
+        if not reply_message:
+            await event.edit("Вы должны или написать шото, или ответить на шото")
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message() 
+        if not reply_message.text:
+            await event.edit("```Ты на текст должен ответить, диб*ил```")
+            return
     chat = "@aleksobot"
     sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
-       return
     await event.edit("```Происходит магия Деменкопа```")
     async with event.client.conversation(chat) as conv:
           try:     
